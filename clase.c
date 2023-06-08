4e0d0e326edbacbb30c74b46ab6e35fa980393b7
Victor Coding <135385907+Manuel097@users.noreply.github.com>
2023-06-07 18:19:03 -0600

    Add files via upload

---
+++ clase.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+ 
+
+int main(){
+
+  int valor =5 ;
+
+  int *p;
+
+  int **pp;
+
+  p=&valor;
+
+  pp=&p;
+
+  
+
+  printf("Contenido en memoria: %8d  Direccion de memoria: %p  Tipo de dato: int\n", valor, &valor );
+
+  printf("Contenido en memoria: %p  Direccion de memoria: %p  Tipo de dato: int*\n",p,&p);
+
+  printf("Contenido en memoria: %p  Direccion de memoria: %p  Tipo de dato: int**\n",pp,&pp);
+
+  printf("\n\nEl valor al que apunta mi puntero a entero es: %d",*p);
+
+}
